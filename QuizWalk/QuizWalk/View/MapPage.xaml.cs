5807deb050ef4161b8c71fb238aecdd8cfec1264
imp <johannes.san@gmail.com>
2014-01-09 14:53:40 +0100

    added method to change the letters

--- QuizWalk/QuizWalk/View/MapPage.xaml.cs
+++ QuizWalk/QuizWalk/View/MapPage.xaml.cs
@@ -11,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -32,6 +33,68 @@ namespace QuizWalk
         public static MapPage getInstance()
         {
             return instance != null ? instance : (instance = new MapPage());
+        }
+
+        /// <summary>
+        /// Method to set the image in the columns
+        /// </summary>
+        /// <param name="column"></param>
+        ///     int must be between 0 (inclusive) and 15 (inclusive).
+        /// <param name="letter"></param>
+        ///     any 1 char letter or 'empty' is allowed.
+        public void changeLetter(int column, string letter)
+        {
+            column++;
+            switch(column)
+            {
+                case 1:
+                    i1st.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 2:
+                    i2nd.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 3:
+                    i3rd.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 4:
+                    i4th.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 5:
+                    i5th.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 6:
+                    i6th.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 7:
+                    i7th.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 8:
+                    i8th.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 9:
+                    i9th.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 10:
+                    i10th.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    i11th.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 13:
+                    i12th.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 14:
+                    i13th.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 15:
+                    i14th.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+                case 16:
+                    i15th.Source = new BitmapImage(new Uri(this.BaseUri, "ms-appx:/Assets/images/Letters/" + letter + ".png"));
+                    break;
+            }
         }
     }
 }
